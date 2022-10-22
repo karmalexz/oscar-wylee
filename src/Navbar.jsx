bb11765f24bbf389d5320167fecd92a17631a58c
karmalexz <hellozhangdi1994@gmail.com>
2022-10-23 02:11:53 +1100

    navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "./assets/OW-Logo-RGB.svg";
+import Search from "./assets/search.svg";
+import User from "./assets/user.svg";
+import ShoppingCart from "./assets/shopping.svg";
+
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={Logo} className="logo" alt="Oscar Wylee logo" />
+      <ul className="menu">
+        <li>Glasses</li>
+        <li>Sunglasses</li>
+        <li>Locations</li>
+        <li>Eye Test Info</li>
+        <li>Offers</li>
+      </ul>
+      <button className="booking">Book an eye test</button>
+      <img className="function-icon" src={Search} alt="search icon" />
+      <img className="function-icon" src={User} alt="user icon" />
+      <img className="function-icon" src={ShoppingCart} alt="shopping cart" />
+      <span className="cart-lable">2</span>
+    </nav>
+  );
+};
+
+export default Navbar;
