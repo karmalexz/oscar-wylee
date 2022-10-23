b8cbe7e603ef2b372c86f2b5fbeaa381cbd139fd
karmalexz <hellozhangdi1994@gmail.com>
2022-10-23 20:05:00 +1100

    add cart number ui

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,13 +19,14 @@ const Navbar = () => {
       <button className="booking">Book an eye test</button>
       <img className="function-icon" src={Search} alt="search icon" />
       <img className="function-icon" src={User} alt="user icon" />
-
+      {/* <div> */}
       <img
         className="function-icon"
         src={ShoppingCart}
         alt="shopping cart"
       ></img>
-      {/* <span className="cart-lable">2</span> */}
+      <span className="cart-lable">2</span>
+      {/* </div> */}
     </nav>
   );
 };
